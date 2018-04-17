871e548528a30b8c072bc28c1fbadc3b61223856
Euan <bell_designs@outlook.com>
2018-04-17 17:01:41 +0100

    set up river, fish and bear files and spec files

---
+++ fish.rb
@@ -0,0 +1,9 @@
+class fish
+
+def initialize(:name)
+  @fish_name = fish_name
+end
+
+
+
+end
---
+++ specs/fish_specs.rb
@@ -0,0 +1,21 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../fish.rb")
+require_relative("../river.rb")
+
+class FishTest < MiniTest:Test
+
+  def setup
+    @fish = fish.new("qwerty")
+  end
+
+
+  def test_fish_name
+
+  end
+
+
+
+
+
+end
